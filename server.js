6390831245ce6954fec0f7d06035a619cb340014
diemisdime <110165774+diemisdime@users.noreply.github.com>
2022-09-18 00:10:10 +0800

    added weather-js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+var weather = require('weather-js');
+
+ 
+weather.find({search: 'Davao City, PH', degreeType: 'C'}, function(err, result) {
+  if(err) console.log(err);
+ 
+  console.log(JSON.stringify(result, null, 2));
+});
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello world</h1>')
