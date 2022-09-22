53e620e0dd7c08bb18899687e787467752e4040f
diemisdime <110165774+diemisdime@users.noreply.github.com>
2022-09-22 21:50:58 +0800

    server.js, index.ejs, and other.ejs modified

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 console.log(__dirname);
 
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.sendFile('/views/index.ejs', {root: __dirname});
+  res.render('index');
+});
+
+app.get('/other', function (req, res) {
+  res.render('other');
+});
+
+app.use((req,res) => {
+  res.render('404');
 });
 
 app.listen(3000);
