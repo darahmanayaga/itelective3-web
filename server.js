0e225774e4bca33a417e34927f273f0b422085fd
diemisdime <110165774+diemisdime@users.noreply.github.com>
2022-09-22 21:28:53 +0800

    server.js and index.ejs edited

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const app = express();
-var weather = require('weather-js');
-
- 
-weather.find({search: 'Davao City, PH', degreeType: 'C'}, function(err, result) {
-  if(err) console.log(err);
- 
-  console.log(JSON.stringify(result, null, 2));
-});
+console.log(__dirname);
 
 app.get('/', function (req, res) {
-  res.send('<h1>Hello world</h1>')
-})
+  res.sendFile('/views/index.html', {root: __dirname});
+});
 
 app.listen(3000);
