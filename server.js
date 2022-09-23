b587db1e27d9813a891c9b36fec49173920374ef
diemisdime <110165774+diemisdime@users.noreply.github.com>
2022-09-23 23:43:18 +0800

    idk na

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 console.log(__dirname);
+var weather = require('weather-js');
+ 
+weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result) {
+  if(err) console.log(err);
+ 
+  console.log(JSON.stringify(result, null, 2));
+});
 
 app.set('view engine', 'ejs');
 
