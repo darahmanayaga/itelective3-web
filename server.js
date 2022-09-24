a684122a3bc5b4197b94a9acbc43c5cda62c6a99
diemisdime <110165774+diemisdime@users.noreply.github.com>
2022-09-24 19:39:46 +0800

    rendered data

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const express = require('express');
 const app = express();
 console.log(__dirname);
 var weather = require('weather-js');
- 
-weather.find({search: 'San Francisco, CA', degreeType: 'F'}, function(err, result) {
-  if(err) console.log(err);
- 
-  console.log(JSON.stringify(result, null, 2));
-});
 
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.render('index');
+
+  weather.find({search: 'Davao City, PH', degreeType: 'C'}, function(err, result) {
+    var weather_data = null;
+    if(err) console.log(err);
+    else{
+    // console.log(JSON.stringify(result, null, 2));
+        weather_data = result;
+
+    };
+    res.render('index', { weather: weather_data });
+  });
+
 });
+
 
 app.get('/other', function (req, res) {
   res.render('other');
