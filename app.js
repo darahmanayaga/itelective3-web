761f3ce9c2c4d72dc1f2c7e367c3d62547f54156
diemisdime <110165774+diemisdime@users.noreply.github.com>
2022-10-24 18:12:03 +0800

    modified app.js, package.json, and items.ejs

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 var express = require('express');
 var app = express();
-app.use(express.static('assets'));
+//  app.use(express.static('assets'));
 
 const port = process.env.PORT || 3000;
 
 app.listen(port,);
 app.set('view engine', 'ejs');
 
+var fs = require("firebase-admin");
+let serviceAccount;
+if (process.env.GOOGLE_CREDENTIALS != null) {
+    serviceAccount = JSON.parse(process.env.GOOGLE_CREDENTIALS)
+}
+else {
+    serviceAccount = require("./premiumluxuriouswines-firebase-adminsdk-qjn4q-cc318968ea.json")
+}
+fs.initializeApp({
+    credential: fs.credential.cert(serviceAccount)
+});
+
+
+const db = fs.firestore();
+const itemColl = db.collection('items');
+
 app.get('/', function(req, res) {
     res.render('index');
 });
 
 app.get('/items', async function(req, res) {
+    const items = await itemColl.get();
+    console.log(items.docs.length);
     let data = {
-        url: req.url
+        url: req.url,
+        itemData: items.docs,
     }
     res.render('items', data);
 });
